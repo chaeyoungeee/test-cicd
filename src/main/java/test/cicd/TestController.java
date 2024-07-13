dc6aeaa4b3cc072a2b8bddeca1fc9a73f288c498
chaeyoungeee <chaeinge@gmail.com>
2024-07-14 02:30:11 +0900

    text change test

--- src/main/java/test/cicd/TestController.java
+++ src/main/java/test/cicd/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String test() {
-        return "success";
+        return "test~~~~success";
     }
 }
