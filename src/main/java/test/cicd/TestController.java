2f88302a3afff17014fe8e8d8afc86fe8ca75411
chaeyoungeee <chaeinge@gmail.com>
2024-07-14 09:54:38 +0900

    test~~~

--- src/main/java/test/cicd/TestController.java
+++ src/main/java/test/cicd/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String test() {
-        return "test~~~~success";
+        return "test~~~~success!!!!!";
     }
 }
